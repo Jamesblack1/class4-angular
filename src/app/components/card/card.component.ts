f1986b5fc4b4e82299e77afdde974532ef6503e0
Jaime De la Fuente <jpdelafuenteba@gmail.com>
2022-01-26 21:19:38 -0300

    first commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Game } from "../games/games.component";
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input() dataInput:Game = {name:"",description:"",platform:"",img:""};
+  @Input() indexInput: number =0;
+  @Output() selectGame: EventEmitter<any>;
+
+
+  constructor() { 
+    this.selectGame = new EventEmitter();
+  }
+
+  ngOnInit(): void {
+  }
+
+  buyGame(){
+    console.log(this.indexInput);
+    this.selectGame.emit(this.indexInput);
+  }
+
+}
